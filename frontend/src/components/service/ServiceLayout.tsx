c190c05c3780ad63b74452f3b7d09e45f0696261
rodgas <rodriguegasore11@gmail.com>
2025-10-09 14:18:42 +0300

    fix: Ajout SidebarProvider dans ServiceLayout pour SidebarTrigger

--- frontend/src/components/service/ServiceLayout.tsx
+++ frontend/src/components/service/ServiceLayout.tsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
 import ServiceSidebar from "./ServiceSidebar";
 import Header from "../Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 const ServiceLayout = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <div className="flex pt-16">
-        <ServiceSidebar />
-        <main className="flex-1 p-6 lg:ml-64">
-          <Outlet />
-        </main>
+    <SidebarProvider>
+      <div className="min-h-screen bg-background">
+        <Header />
+        <div className="flex pt-16">
+          <ServiceSidebar />
+          <main className="flex-1 p-6 lg:ml-64">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
